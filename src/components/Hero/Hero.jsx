4c9691886d7c7ab5daf43fb416124eaf7aaae224
Tatiana Zaidel <8tzaidel@gmail.com>
2024-06-01 19:39:57 +0200

    create template for project

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section>
+      <h1>no name shop</h1>
+    </section>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HomePage from './pages/HomePage/HomePage'
+import './index.css'
+import 'modern-normalize';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>
+);
