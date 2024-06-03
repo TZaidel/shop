9e3b63d0a2a3a6031712c3e2bdd44156df18d95a
Serhii-Fesianov <littlefox06042000@gmail.com>
2024-06-03 15:25:50 +0300

    add header component
    basic structure without styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,15 @@
 import Hero from '../../components/Hero/Hero';
 import ProductList from '../../components/ProductList/ProductList';
 import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
 
 export default function HomePage() {
   return (
     <div>
+      <Header />
       <Hero />
       <ProductList />
-      <Footer/>
-      
+      <Footer />
     </div>
   );
 }
