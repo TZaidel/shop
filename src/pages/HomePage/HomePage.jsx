4abd1f54dd7f82510c40be6c5669a2a45784a71d
IuriiRiabeka <sam2fly7@gmail.com>
2024-06-03 13:26:28 +0300

    Correct ask

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,14 @@
 import Hero from '../../components/Hero/Hero';
+import ProductList from '../../components/Hero/ProductList/ProductList';
+import Footer from '../../components/Hero/Footer/Footer';
 
 export default function HomePage() {
   return (
     <div>
       <Hero />
+      <ProductList />
+      <Footer/>
+      
     </div>
   );
 }
