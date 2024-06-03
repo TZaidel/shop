effa453308a078a4c788e1555b67112f4f9d451f
Tatiana Zaidel <8tzaidel@gmail.com>
2024-06-03 12:57:29 +0200

    edit import

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import Hero from '../../components/Hero/Hero';
-import ProductList from '../../components/Hero/ProductList/ProductList';
-import Footer from '../../components/Hero/Footer/Footer';
+import ProductList from '../../components/ProductList/ProductList';
+import Footer from '../../components/Footer/Footer';
 
 export default function HomePage() {
   return (
