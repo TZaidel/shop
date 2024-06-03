0c7f9e2dd3c45337d11078b5cb5c74fff2e76a2e
IuriiRiabeka <sam2fly7@gmail.com>
2024-06-03 13:16:34 +0300

    base of main section and footer

---
+++ src/components/Hero/ProductList/ProductList.jsx
@@ -0,0 +1,17 @@
+export default function ProductList() {
+  return (
+    <section>
+      <p> Product LIst </p>
+      <ul>
+        <li>Product N1</li>
+        <li>Product N2</li>
+        <li>Product N3</li>
+        <li>Product N4</li>
+        <li>Product N5</li>
+        <li>Product N6</li>
+        <li>Product N7</li>
+        <li>Product N8</li>
+      </ul>
+    </section>
+  )
+}
