cfc77a919694ac21584a7436423d189eb76a4fab
Tatiana Zaidel <8tzaidel@gmail.com>
2024-06-20 14:27:10 +0200

    edit styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,9 +3,11 @@ import Hero from '../../components/Hero/Hero';
 import ProductList from '../../components/ProductList/ProductList';
 import Footer from '../../components/Footer/Footer';
 
+import baseStyle from '../Base.module.css';
+
 export default function HomePage() {
   return (
-    <div>
+    <div className={baseStyle.container}>
       <Header />
       <h1>Home Page</h1>
       <Hero />
