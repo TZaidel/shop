37abc30fa2ffef92106664f603e50ba933a04a31
Serhii-Fesianov <littlefox06042000@gmail.com>
2024-06-20 18:11:47 +0300

    login-page desktop styles

--- src/pages/LogInPage/LogInPage.jsx
+++ src/pages/LogInPage/LogInPage.jsx
@@ -1,7 +1,54 @@
+import { Link } from 'react-router-dom';
+import Header from '../../components/Header/Header';
+import styles from './LoginPage.module.css';
+
 export default function LogInPage() {
   return (
     <>
-      <h1>LogInPage</h1>
+      <Header />
+      <div className={styles.boxForWrappers}>
+        <div className={styles.wraperFormAuth}>
+          <h2 className={styles.titleLogin}>ВХІД</h2>
+          <form>
+            <div className={styles.boxInputs}>
+              <input
+                className={styles.inputStyles}
+                type="email"
+                placeholder="електронна пошта *"
+              />
+              <input
+                className={styles.inputStyles}
+                type="password"
+                placeholder="пароль *"
+              />
+            </div>
+            <div className={styles.boxCheckboxAndLink}>
+              <label htmlFor="agree" className={styles.boxLabel}>
+                <input type="checkbox" id="agree" name="agree" />
+                <p className={styles.textRememberData}>
+                  Запам&apos;ятати введені дані
+                </p>
+              </label>
+              <Link to="/restoration" className={styles.restorationLink}>
+                Забули пароль?
+              </Link>
+            </div>
+          </form>
+          <button className={styles.buttonLogin}>
+            увійти до особистого кабінету
+          </button>
+        </div>
+        <div className={styles.rectigleStyles}></div>
+        <div className={styles.boxNavToRegister}>
+          <h2 className={styles.titleLogin}>ЗАРЕЄСТРУЙТЕСЯ ЗАРАЗ</h2>
+          <p className={styles.registerText}>
+            Зареєструйтеся для оформлення замовлення
+          </p>
+          <Link to="/register" className={styles.navLinkRegister}>
+            ЗАРЕЄСТРУВАТИСЬ
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
