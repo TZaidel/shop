e1104ba2b69579bc9995a20064933cdddf24f7f4
Serhii-Fesianov <littlefox06042000@gmail.com>
2024-07-22 19:38:23 +0300

    stopPropagation on modal and styles tablet 768px

--- src/components/BurgerMenuModal/BurgerMenuModal.jsx
+++ src/components/BurgerMenuModal/BurgerMenuModal.jsx
@@ -1,18 +1,50 @@
 import styles from './BurgerMenuModal.module.css';
 import PropTypes from 'prop-types';
+import { BsBasket3 } from 'react-icons/bs';
+import { GoPerson } from 'react-icons/go';
+import { IoClose, IoSearchSharp } from 'react-icons/io5';
 
 const BurgerMenuModal = ({ isOpened, onClose }) => {
   if (!isOpened) return null;
+
+  const handleWrapperClick = event => {
+    event.stopPropagation();
+  };
 
   return (
     <div className={styles.specialBox}>
       <div className={styles.backDrop} onClick={onClose}>
         <div
           className={`${styles.wrapperModal} ${isOpened ? styles.active : ''}`}
+          onClick={handleWrapperClick}
         >
           <button className={styles.buttonClose} onClick={onClose}>
-            Закрити
+            <IoClose className={styles.closeIcon} />
           </button>
+
+          <ul className={styles.listBurgerMenu}>
+            <li className={styles.itemBurgerMenu}>
+              <p>ЖІНКАМ</p>
+            </li>
+            <li className={styles.itemBurgerMenu}>
+              <p>ЧОЛОВІКАМ</p>
+            </li>
+            <li className={styles.itemBurgerMenu}>
+              <p>ЗНИЖКИ</p>
+            </li>
+            <li className={styles.itemBurgerMenu}>
+              <p>ПОШУК</p>
+              <IoSearchSharp className={styles.IconsToNavSearch} />
+            </li>
+            <li className={styles.itemBurgerMenu}>
+              <p>ПРОФІЛЬ</p>
+              <GoPerson className={styles.IconsToNavPerson} />
+            </li>
+            <li className={styles.itemBurgerMenuBasket}>
+              <p>КОШИК</p>
+              <BsBasket3 className={styles.IconsToNavBasket} />
+            </li>
+          </ul>
         </div>
       </div>
     </div>
