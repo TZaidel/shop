28806f204da0da900fce91e46883f4ae5a2d1853
Tatiana Zaidel <8tzaidel@gmail.com>
2024-09-06 09:59:02 +0200

    fix nav links and styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
-import styles from "./Footer.module.css"
+import { Link } from 'react-router-dom';
+
+import styles from './Footer.module.css';
 export default function Footer() {
   return (
     <section className={styles.footerSection}>
-     <p >UA TRADITIONS
-      </p>
-   
+      <Link to="/" className={styles.logo}>
+        UA TRADITIONS
+      </Link>
     </section>
-  )
+  );
 }
